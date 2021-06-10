9e52d7288d50b23e09a01cc6bf92ded8f368ded3
suey0128 <syui0128@gmail.com>
2021-06-09 22:16:37 -0700

    Completed assignment

---
+++ src/components/ArticleList.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Article from "./Article";
+
+function ArticleList ({ posts }) {
+
+    const articleIterator = posts.map((post) => {
+        return (
+            <Article 
+            key = {post.id}
+            title = {post.title}
+            date = {post.date}
+            preview = {post.preview}
+            />
+        )
+    })
+
+
+    return (
+        <main>
+            {articleIterator}
+        </main>
+    )
+}
+
+export default ArticleList;
